fe03e31e8c9ca3032dda33c1847f6d7cd2003551
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-04-12 23:54:09 +0300

    Update SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+UPDATE Departments SET Financing = 35000 WHERE Name = 'Department A';
+UPDATE Diseases SET Severity = 4 WHERE Name = 'Flu';
+UPDATE Doctors SET Premium = 2000 WHERE Surname = 'Doe';
+UPDATE Examinations SET StartTime = '09:00' WHERE Name = 'Physical Exam';
+UPDATE Wards SET [Floor] = 2 WHERE Name = 'Ward 101';
