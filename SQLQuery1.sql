f4889758747b55efc006eb56d15ffc145366a488
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-04-12 23:55:23 +0300

    Update SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+DELETE FROM Departments WHERE Name = 'Department B';
+DELETE FROM Diseases WHERE Name = 'Common Cold';
+DELETE FROM Doctors WHERE Surname = 'Smith';
+DELETE FROM Examinations WHERE Name = 'Blood Test';
+DELETE FROM Wards WHERE Name = 'Ward 102';
